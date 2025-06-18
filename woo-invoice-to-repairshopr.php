8e3de027970c3360b19d42f37c4aa43e5c7d5a4c
radialmonster <radial@gmail.com>
2025-06-18 16:08:53 -0400

    Remove hardcoded plugin version constant

--- woo-invoice-to-repairshopr.php
+++ woo-invoice-to-repairshopr.php
@@ -16,7 +16,6 @@ if (!defined('ABSPATH')) {
 define('WIR_PLUGIN_FILE', __FILE__);
 define('WIR_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WIR_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('WIR_PLUGIN_VERSION', '1.11');
 
 /**
  * Main plugin initialization
